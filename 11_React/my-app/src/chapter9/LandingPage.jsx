738fd3d2f5b573a856ef12565d96a2ca2806b772
lis2s2 <hyeon6895@naver.com>
2024-04-30 16:50:31 +0900

    리액트 너무 어려움

--- 11_React/my-app/src/chapter9/LandingPage.jsx
+++ 11_React/my-app/src/chapter9/LandingPage.jsx
@@ -3,20 +3,27 @@ import Toolbar from './Toolbar';
 
 function LandingPage(props) {
   // 사용자의 로그인 여부를 상태로 관리
-  const {isLoggedIn, setIsLoggedIn} = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleClickLogin = () => {
     setIsLoggedIn(true);
-  };
+  }
 
   const handleClickLogout = () => {
     setIsLoggedIn(false);
-  };
+  }
+
 
   return (
     <>
-      <Toolbar />
-      <div style={{padding: 16}}>리액트 공부 사이트</div>
+      <Toolbar
+       // Quiz: 로그인 상태와 이벤트 핸들러를 툴바 컴포넌트로 넘겨 로그인 여부에 따라 툴바의 렌더링이 바뀌도록 구현
+      isLoggedIn = {isLoggedIn}
+      onClickLogin = {handleClickLogin}
+      onClickLogout = {handleClickLogout}
+      />
+      
+      <div style={{padding: 16}}>♥리액트 공부 사이트♥</div>
     </>
   );
 }
